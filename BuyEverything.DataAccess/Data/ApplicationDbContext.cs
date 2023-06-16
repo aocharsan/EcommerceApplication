041da1e86a13084df23572ada321d88b021918ea
Sanket <aocharsanket@gmail.com>
2023-06-16 11:32:56 +0530

    ASP.Net Core MVC with .dll references

---
+++ BuyEverything.DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,38 @@
+﻿using BuyEverything.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BuyEverything.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+       
+        
+        
+       
+                
+        }
+        //create a columns in Categories table
+        public DbSet<Category> Categories { get; set; }
+
+        //VIMP -->when anything is to be updated to database ,we have to use add-migration <name> 
+
+
+        //seed data to category table
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+                modelBuilder.Entity<Category>().HasData(
+                new Category { Id=1 ,Name="Action",DisplayOrder=1},
+                new Category { Id =2, Name = "SciFi", DisplayOrder = 2 },
+                new Category { Id =3, Name = "History", DisplayOrder = 3 }
+
+                );
+        }
+
+
+
+
+
+    }
+}
---
+++ BuyEverything.Utiliity/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace BuyEverything.Utility
+{
+    public class Class1
+    {
+
+    }
+}
